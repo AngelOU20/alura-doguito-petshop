7a516439056d3949f43ad234994debff4390c296
Julio Ucharima <julio_ucharima@usmp.pe>
2023-09-08 12:25:04 -0500

    conexión con API utlizando axios

--- src/api/api.js
+++ src/api/api.js
@@ -1,3 +1,10 @@
 import axios from "axios";
 
-axios;
+export const api = axios.create({
+  baseURL: "http://localhost:5000",
+});
+
+export const buscar = async (url, setData) => {
+  const resp = await api.get(url);
+  setData(resp.data);
+};
