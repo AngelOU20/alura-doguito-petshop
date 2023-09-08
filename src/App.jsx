2a715520246e4c9d959eb0f5bdde4bc142202b40
Julio Ucharima <julio_ucharima@usmp.pe>
2023-09-08 14:11:10 -0500

    Quitando clg de App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,6 @@ import { AppRouter } from "./routes/AppRouter";
 import { BrowserRouter } from "react-router-dom";
 
 function App() {
-  const location = window.location;
-  console.log(location);
-
   return (
     <BrowserRouter>
       <AppRouter />
