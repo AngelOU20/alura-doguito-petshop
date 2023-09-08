1e122d408c042d5164714ddb3f188cded2e405a8
Julio Ucharima <julio_ucharima@usmp.pe>
2023-09-08 12:50:56 -0500

    Mostrando componente PostCard en ListPost

--- src/components/ListPost.jsx
+++ src/components/ListPost.jsx
@@ -1,9 +1,24 @@
+import { useEffect, useState } from "react";
+import { buscar } from "../api/api";
 import "../assets/css/componentes/card.css";
+import PropTypes from "prop-types";
+import { PostCard } from "./";
 
-export const ListPost = () => {
+export const ListPost = ({ url }) => {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    buscar(url, setPosts);
+  }, [url]);
+
   return (
-    <section className="post container">
-      <h1>Post</h1>
+    <section className="posts container">
+      {posts.map((post) => (
+        <PostCard key={post.id} {...post} />
+      ))}
     </section>
   );
+};
+
+ListPost.propTypes = {
+  url: PropTypes.string,
 };
