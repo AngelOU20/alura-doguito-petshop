78855358b96dde05a2a7e53f5e1063593688733f
Julio Ucharima <julio_ucharima@usmp.pe>
2024-06-20 11:38:51 -0500

    add styles

--- src/pages/Page404.jsx
+++ src/pages/Page404.jsx
@@ -5,7 +5,12 @@ export const Page404 = () => {
   return (
     <main className="container flex flex--center flex--column error-page">
       <h1 className="title">Error 404</h1>
-      <img className="dog-image" src={image} alt="Doguito" />
+      <img
+        className="dog-image"
+        src={image}
+        style={{ width: "60%" }}
+        alt="Doguito"
+      />
       <p className="notfound-text">Esta página no existe</p>
     </main>
   );
